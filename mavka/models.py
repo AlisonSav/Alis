58f9a93b098122019b1a926e1f06f7c3b935d849
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-06-19 16:56:03 +0200

    add models

--- mavka/models.py
+++ mavka/models.py
@@ -1,3 +1,37 @@
-# from django.db import models
+"""In the forest there is one Keeper and many creatures"""
+from django.db import models # noqa: F401
 
-# Create your models here.
+
+class Forest(models.Model):
+    title = models.CharField(max_length=20, unique=True)
+    earth = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Forest: {self.title}, {self.earth}"
+
+
+class Keeper(models.Model):
+    keeper_name = models.CharField(max_length=20)
+    forest = models.OneToOneField(Forest, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Keeper: {self.keeper_name}, Forest: {self.forest}"
+
+
+class Food(models.Model):
+    food_title = models.CharField(max_length=20)
+    energy = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Food: {self.food_title}"
+
+
+class Creature(models.Model):
+    creature_name = models.CharField(max_length=20)
+    color = models.CharField(max_length=10)
+    age = models.IntegerField()
+    forest = models.ForeignKey(Forest, on_delete=models.SET_NULL, blank=True, null=True)
+    food = models.ManyToManyField(Food)
+
+    def __str__(self):
+        return f"Creature: {self.creature_name}, {self.age}, Forest: {self.forest}"
